40ff1b8f23a1b00b2bc4faf88a70fa53acaec06a
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-11-24 11:09:51 +0800

    fixes

--- Algortihm/BubbleSort.c
+++ Algortihm/BubbleSort.c
@@ -2,10 +2,12 @@
 #include <stdbool.h>
 
 void bubblesort(int arr[], int size){
+
     bool swapped;
 
     for(int i=0; i<size-1; ++i){
         swapped = false;
+
         for(int j=0; j<size-1-i; ++j){
             if(arr[j] > arr[j+1]){
                 arr[j] ^= arr[j+1];
@@ -14,6 +16,9 @@ void bubblesort(int arr[], int size){
                 swapped = true;
             }
         }
-        if(!swapped)break;
+
+        if(!swapped){
+            break;
+        }
     }
 }
--- Algortihm/QuickSort.c
+++ Algortihm/QuickSort.c
@@ -12,14 +12,18 @@ int partition(int arr[], int low, int high){
             arr[j] = temp;
         }
     }
+
     int temp = arr[i + 1];
     arr[i + 1] = arr[high];
     arr[high] = temp;
+
     return i+1;
 }
 
 void quicksort(int arr[], int low, int high){
+
     if(low< high){
+
         int pi = partition(arr, low, high);
 
         quicksort(arr, low, pi-1);
