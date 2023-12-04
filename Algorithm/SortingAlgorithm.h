fa7f24f294ba335d354094bda68b199546e07b42
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-12-04 13:43:08 +0800

    Update SortingAlgorithm.h
    add files

--- Algorithm/SortingAlgorithm.h
+++ Algorithm/SortingAlgorithm.h
@@ -1,3 +1,5 @@
 void quicksort(int arr[], int low, int high);
 void bubblesort(int arr[], int size);
 void mergesort(int arr[], int start, int end);
+void selectionSort(int arr[], int n);
+void maxmin(int arr[], int i, int j, int *max, int *min);
