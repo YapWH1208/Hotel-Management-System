4fb1856ba6d5edaf614ad2e805961d3a0c4a5b7c
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-12-06 17:34:25 +0800

    fixes

--- Algorithm/SearchingAlgorithm.h
+++ Algorithm/SearchingAlgorithm.h
@@ -1,2 +1,2 @@
 int binarySearch(int id, int low, int high);
-
+void maxmin(int arr[], int i, int j, int *max, int *min);
--- Algorithm/SortingAlgorithm.h
+++ Algorithm/SortingAlgorithm.h
@@ -2,4 +2,3 @@ void quicksort(int arr[], int low, int high);
 void bubblesort(int arr[], int size);
 void mergesort(int arr[], int start, int end);
 void selectionSort(int arr[], int n);
-void maxmin(int arr[], int i, int j, int *max, int *min);
